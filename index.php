40c044ba529659ba469ecae52c5f4c626659c47a
Valak264 <gaelicius@gmail.com>
2024-05-05 16:28:54 +0700

    Menambahkan batas jumlah pembelian

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once 'config.php';
 $stmt = $con->prepare("SELECT * FROM product");
 $stmt->execute();
 $result = $stmt->get_result();
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +13,16 @@ $result = $stmt->get_result();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+      function limit(qValue) {
+        let limit = qValue;
+        if (limit > 1000) {
+          document.getElementById("quantity").value = 1000;
+        } else if (limit <= 0) {
+          document.getElementById("quantity").value = 1;
+        }
+      }
+    </script>
     <title>Home</title>
     <style>
       .img-sizing {
@@ -30,12 +41,18 @@ $result = $stmt->get_result();
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
+          <a class="nav-link text-white active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link text-white" href="#">About Us</a>
         </li>
       </ul>
+      <div class="me-2 ms-2">
+        <a class="nav-link text-white" href="#">Register</a>
+      </div>
+      <div class="me-2 ms-2">
+        <a class="nav-link text-white" href="#">Login</a>
+      </div>
     </div>
   </div>
 </nav>
@@ -46,15 +63,6 @@ $result = $stmt->get_result();
       <h1 class="text-center">Welcome to our simple bookstore !</h1>
     </div>
     <div class="row row-cols-1 row-cols-md-3 g-4">
-      <!-- <div class="col">
-        <div class="card h-100">
-          <img src="images/2729462.jpg" class="card-img-top img-sizing" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text"></p>
-          </div>
-        </div>
-      </div> -->
       <?php if ($result->num_rows > 0) { 
         while ($data = $result->fetch_assoc()) { ?>
       <div class="col">
@@ -64,11 +72,20 @@ $result = $stmt->get_result();
             <h5 class="card-title"><?php echo $data['product_name'];?></h5>
             <p class="card-text"><?php echo $data['description'];?><br/>
               Author : <?php echo $data['author'];?><br/>
-              Price : <?php echo $data['price'];?><br/>
+              Price : Rp. <?php echo number_format($data['price']);?><br/>
               Stock Available : <?php echo $data['quantity'];?><br/>
             </p>
-            <a href="" class="btn btn-success">Buy</a>
-            <a href="" class="btn btn-warning">Add to Cart</a>
+
+            <form action="checkout/" method="get">
+              <div class="mb-3">
+                <label for="quantity" class="form-label">Quantity</label>
+                <input type="hidden" name="id" value="<?php echo $data['id']?>">
+                <input type="number" name="quantity" id="quantity" value="1" min="1" max="<?php echo $data['quantity'];?>" class="form-control" oninput="limit(this.value)" required>
+                <br/>
+                <input type="submit" class="btn btn-success" name="buy" id="submit-btn" value="Buy">
+                <input type="submit" class="btn btn-warning" name="addtocart" id="submit-btn" value="Add to Cart">
+              </div>
+            </form>
           </div>
         </div>
       </div>
